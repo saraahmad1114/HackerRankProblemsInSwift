f1985a2171e104ffb01294c37b871cffa8afad93
Sara <saraahmad1114@yahoo.com>
2016-11-29 22:00:47 -0500

    first problem done

--- HackerRankAlgorithmProblems.playground/Contents.swift
+++ HackerRankAlgorithmProblems.playground/Contents.swift
@@ -3,3 +3,22 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//HackerRank Problem: Given an array of  integers, can you find the sum of its elements
+
+
+func findSumOfAllNumbersInArray (numbers: [Int]) -> Int {
+
+    var value = 0
+    
+    for num in numbers {
+        
+        value += num
+    }
+
+    return value
+}
+
+var randomNumbers = [7, 8, 9, 10]
+
+findSumOfAllNumbersInArray(randomNumbers)
